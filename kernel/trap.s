2cd90de0a6debc0b071789732e0c19e5e4bd2ca5
icexin <fanbingxin.me@gmail.com>
2020-10-24 17:01:08 +0800

    all: format asm file

--- kernel/trap.s
+++ kernel/trap.s
@@ -1,44 +1,45 @@
 #include "textflag.h"
 
-TEXT ·idt_init(SB),NOSPLIT,$0
-	CALL	·fillidt(SB)
-	LIDT	·idtptr(SB)
+TEXT ·idt_init(SB), NOSPLIT, $0
+	CALL ·fillidt(SB)
+	LIDT ·idtptr(SB)
 	RET
 
 #define m_stack 0
 #define m_tf 4
 
-TEXT alltraps(SB),NOSPLIT,$0
+TEXT alltraps(SB), NOSPLIT, $0
 	// 因为我们没有切换ring，因此需要显式保存一下sp指针，
 	// 便于在中断处理程序中修改sp
-	PUSHL	SP
+	PUSHL SP
 	PUSHAL
-	PUSHW	DS
-	PUSHW	ES
-	PUSHW	FS
-	PUSHW	GS
+	PUSHW DS
+	PUSHW ES
+	PUSHW FS
+	PUSHW GS
 
-	MOVL 	0(FS), CX // CX store mythread
-	MOVL	SP, m_tf(CX)
+	MOVL 0(FS), CX    // CX store mythread
+	MOVL SP, m_tf(CX)
 
 	// call trap(tf)
-	PUSHL	SP
-	CALL	·dotrap(SB)
-	ADDL	$4, SP
-	JMP		·trapret(SB)
+	PUSHL SP
+	CALL  ·dotrap(SB)
+	ADDL  $4, SP
+	JMP   ·trapret(SB)
 
-TEXT ·trapret(SB),NOSPLIT,$0
-	MOVL 	0(FS), CX // CX store mythread
-	MOVL	$0, m_tf(CX) // clear tf
+TEXT ·trapret(SB), NOSPLIT, $0
+	MOVL 0(FS), CX    // CX store mythread
+	MOVL $0, m_tf(CX) // clear tf
 
-	POPW	GS
-	POPW	FS
-	POPW	ES
-	POPW	DS
+	POPW GS
+	POPW FS
+	POPW ES
+	POPW DS
 	POPAL
-	POPL	SP
+	POPL SP
 
-	ADDL	$8, SP // skip trapno and errcode
+	ADDL $8, SP // skip trapno and errcode
+
 	// IRET
 	BYTE $0xCF
 
--- mm/asm.s
+++ mm/asm.s
@@ -1,19 +1,18 @@
 #include "textflag.h"
 
-#define CR0_WP          0x00010000      // Write Protect
-#define CR0_PG          0x80000000      // Paging
+#define CR0_WP          0x00010000 // Write Protect
+#define CR0_PG          0x80000000 // Paging
 
-TEXT ·page_enable(SB),NOSPLIT,$0-0
+TEXT ·page_enable(SB), NOSPLIT, $0-0
 	// enable page
-	MOVL	CR0, AX
-	ORL		$(CR0_WP|CR0_PG), AX
-	MOVL	AX, CR0
+	MOVL CR0, AX
+	ORL  $(CR0_WP|CR0_PG), AX
+	MOVL AX, CR0
 	RET
 
-TEXT ·lcr3(SB),NOSPLIT,$0-4
+TEXT ·lcr3(SB), NOSPLIT, $0-4
 	// setup page dir
-	MOVL	pgdir+0(FP), AX
-	MOVL	AX, CR3
+	MOVL pgdir+0(FP), AX
+	MOVL AX, CR3
 	RET
-
 
