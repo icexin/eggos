e93e65b64e2e5c2380bb63bb1f040db38260d784
icexin <fanbingxin.me@gmail.com>
2021-07-17 00:38:13 +0800

    replace syscall const

--- kernel/isyscall/syscall.go
+++ kernel/isyscall/syscall.go
@@ -6,11 +6,6 @@ import (
 	_ "unsafe"
 )
 
-const (
-	_SYS_futex  = 240
-	_FUTEX_WAKE = 1
-)
-
 var (
 	handlers [512]Handler
 )
@@ -30,7 +25,6 @@ type Request struct {
 
 func (r *Request) Done() {
 	wakeup(&r.Lock, 1)
-	// syscall.Syscall6(_SYS_futex, uintptr(unsafe.Pointer(&r.Lock)), _FUTEX_WAKE, 1, 0, 0, 0)
 }
 
 func GetHandler(no uintptr) Handler {
