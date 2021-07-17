4813c607d28423a3be0665e6e5b3efbfbaacfd0f
fanbingxin <fanbingxin.me@gmail.com>
2021-07-18 00:54:44 +0800

    print kernel panic (#42)

--- debug/log.go
+++ debug/log.go
@@ -5,6 +5,8 @@ import (
 	"time"
 
 	"github.com/icexin/eggos/console"
+	"github.com/icexin/eggos/sys"
+	"github.com/icexin/eggos/uart"
 )
 
 var (
@@ -14,4 +16,22 @@ var (
 
 func Logf(fmtstr string, args ...interface{}) {
 	fmt.Fprintf(console.Console(), fmtstr+"\n", args...)
+}
+
+//go:nosplit
+func PrintStr(s string) {
+	uart.WriteString(s)
+}
+
+const hextab = "0123456789ABCDEF"
+
+//go:nosplit
+func PrintHex(n uintptr) {
+	shift := sys.PtrSize*8 - 4
+	for ; shift > 0; shift = shift - 4 {
+		v := (n >> shift) & 0x0F
+		ch := hextab[v]
+		uart.WriteByte(ch)
+	}
+	uart.WriteByte(hextab[n&0x0F])
 }
