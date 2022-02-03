600311d07bf72ff0406f502e945e0ab57440d089
Robin Eklind <rnd0x00@gmail.com>
2022-02-03 10:02:27 +0100

    kernel/sys: add docs for Go function wrappers of asm instructions

--- kernel/sys/sys_386.s
+++ kernel/sys/sys_386.s
@@ -7,7 +7,7 @@ TEXT ·Outb(SB), NOSPLIT, $0-3
 	OUTB
 	RET
 
-// byte Inb(reg uint16)
+// byte Inb(port uint16)
 TEXT ·Inb(SB), NOSPLIT, $0-5
 	MOVW port+0(FP), DX
 	XORW AX, AX
@@ -22,27 +22,32 @@ TEXT ·Outl(SB), NOSPLIT, $0-8
 	OUTL
 	RET
 
+// uint32 Inl(port uint16)
 TEXT ·Inl(SB), NOSPLIT, $0-8
 	MOVW port+0(FP), DX
 	INL
 	MOVL AX, ret+4(FP)
 	RET
 
+// SetAX(val uint32)
 TEXT ·SetAX(SB), NOSPLIT, $0-4
 	MOVL val+0(FP), AX
 	RET
 
+// uint32 Flags()
 TEXT ·Flags(SB), NOSPLIT, $0-4
 	PUSHFL
 	POPL AX
 	MOVL AX, ret+0(FP)
 	RET
 
+// uint32 Cr2()
 TEXT ·Cr2(SB), NOSPLIT, $0-4
 	MOVL CR2, AX
 	MOVL AX, ret+0(FP)
 	RET
 
+// Fxsave(addr uint32)
 TEXT ·Fxsave(SB), NOSPLIT, $0-4
 	MOVL   addr+0(FP), AX
 	FXSAVE (AX)
--- kernel/sys/sys_amd64.s
+++ kernel/sys/sys_amd64.s
@@ -7,7 +7,7 @@ TEXT ·Outb(SB), NOSPLIT, $0-3
 	OUTB
 	RET
 
-// byte Inb(reg uint16)
+// byte Inb(port uint16)
 TEXT ·Inb(SB), NOSPLIT, $0-9
 	MOVW port+0(FP), DX
 	XORW AX, AX
@@ -22,33 +22,39 @@ TEXT ·Outl(SB), NOSPLIT, $0-8
 	OUTL
 	RET
 
+// uint32 Inl(port uint16)
 TEXT ·Inl(SB), NOSPLIT, $0-12
 	MOVW port+0(FP), DX
 	INL
 	MOVL AX, ret+8(FP)
 	RET
 
+// SetAX(val uint64)
 TEXT ·SetAX(SB), NOSPLIT, $0-8
 	MOVQ val+0(FP), AX
 	RET
 
+// uint64 Flags()
 TEXT ·Flags(SB), NOSPLIT, $0-8
 	PUSHFQ
 	POPQ AX
 	MOVQ AX, ret+0(FP)
 	RET
 
+// uint64 Cr2()
 TEXT ·Cr2(SB), NOSPLIT, $0-8
 	MOVQ CR2, AX
 	MOVQ AX, ret+0(FP)
 	RET
 
+// uint64 CS()
 TEXT ·CS(SB), NOSPLIT, $0-8
 	XORQ AX, AX
 	MOVW CS, AX
 	MOVQ AX, ret+0(FP)
 	RET
 
+// Fxsave(addr uint64)
 TEXT ·Fxsave(SB), NOSPLIT, $0-8
 	MOVQ   addr+0(FP), AX
 	FXSAVE (AX)
