c09e358b5e81b06b88b310e5156cd890db38c7c9
Robin <mewmew@users.noreply.github.com>
2022-02-04 11:05:37 +0100

    kernel/sys: add docs for Go function wrappers of asm instructions (#93)

--- kernel/sys/sys.s
+++ kernel/sys/sys.s
@@ -1,13 +1,16 @@
 #include "textflag.h"
 
+// CLI - Clear Interrupt Flag
 TEXT ·Cli(SB), NOSPLIT, $0
 	CLI
 	RET
 
+// STI - Set Interrupt Flag
 TEXT ·Sti(SB), NOSPLIT, $0
 	STI
 	RET
 
+// HLT - Halt
 TEXT ·Hlt(SB), NOSPLIT, $0
 	HLT
 	RET
