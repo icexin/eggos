4dfc2a199f43b6a8b6695698d48f76efe5d4e3b8
Robin Eklind <rnd0x00@gmail.com>
2022-02-07 14:45:32 +0100

    drivers/qemu: add docs

--- drivers/qemu/qemu.go
+++ drivers/qemu/qemu.go
@@ -1,11 +1,15 @@
+// Package qemu provides utility functions for interacting with the Qemu machine
+// emulator and virtualizer.
 package qemu
 
 import "github.com/icexin/eggos/kernel/sys"
 
 const (
+	// port used for Qemu exit commands.
 	qemuExitPort = 0x501
 )
 
+// Exit sends an exit command to Qemu with the given exit code.
 func Exit(code int) {
 	sys.Outb(qemuExitPort, byte(code))
 }
